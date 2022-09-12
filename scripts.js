6e53a3b8d314ddcffec2bebb8814e00f8f0e8f24
Dvir <dvir1225@gmail.com>
2022-09-12 19:06:59 +0300

    add selectors object

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const selectors = {
+    firstScreen: document.querySelector('.firstScreen'),
+    secondScreen: document.querySelector('.secondScreen'),
+    winnerScreen: document.querySelector('.winnerScreen'),
+    startGameBtn: document.getElementById('startGameBtn'),
+    restartGameBtn: document.getElementById('restartGameBtn'),
+    p1Name: document.getElementById('p1Name').value,
+    p2Name: document.getElementById('p2Name').value,
+    gameCells: document.querySelectorAll('.cell')
+}
