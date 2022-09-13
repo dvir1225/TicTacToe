50b4f52f6dfc7410d56d565ac227a716dd998b9f
Dvir <dvir1225@gmail.com>
2022-09-13 15:29:40 +0300

    add changing screens, player names go to second screen table, add game functionality

--- scripts.js
+++ scripts.js
@@ -2,16 +2,27 @@ const selectors = {
     firstScreen: document.querySelector('.firstScreen'),
     secondScreen: document.querySelector('.secondScreen'),
     winnerScreen: document.querySelector('.winnerScreen'),
+    winnerText: document.getElementById('winnerText'),
     startGameBtn: document.getElementById('startGameBtn'),
     restartGameBtn: document.getElementById('restartGameBtn'),
-    p1Name: document.getElementById('p1Name').value,
-    p2Name: document.getElementById('p2Name').value,
-    gameCells: document.querySelectorAll('.cell')
+    p1Name: document.getElementById('p1Name'),
+    p2Name: document.getElementById('p2Name'),
+    p1NameScoreTable: document.getElementById('p1NameValue'),
+    p2NameScoreTable: document.getElementById('p2NameValue'),
+    gameCells: document.querySelectorAll('.cell'),
+    gameboard: document.querySelector('.gameboard'),
+    p1Score: document.getElementById('p1Score'),
+    p2Score: document.getElementById('p2Score')
 }
 
-
-
 const Game = (() => {
+    selectors.startGameBtn.addEventListener('click', () =>{
+        selectors.firstScreen.classList.remove('show')
+        selectors.secondScreen.classList.add('show')
+        selectors.p1NameScoreTable.innerText = selectors.p1Name.value;
+        selectors.p2NameScoreTable.innerText = selectors.p2Name.value;
+    })
+
     const cellClasses = {
         x: 'x',
         circle: 'circle'
@@ -22,9 +33,12 @@ const Game = (() => {
     }
 
     let gameboard = [];
-    let player1 = selectors.p1Name;
-    let player2 = selectors.p2Name;
+    
     let circleTurn = false;
+    selectors.gameboard.classList.add(cellClasses.x);
+    let p1Score = 0;
+    let p2Score = 0;
+    
 
     const placeMark = (cell, currentClass) => {
         cell.classList.add(currentClass);
@@ -34,20 +48,68 @@ const Game = (() => {
         } else {
             cell.innerHTML = gameMarks.x;
             gameboard.push('x');
+        } cell.style.color = "black";
+    }
+
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3 ,6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    function checkWin(currentClass) {
+        return winningCombinations.some(combination =>{
+            return combination.every(index => {
+              return selectors.gameCells[index].classList.contains(currentClass);
+            })
+        })
+    }
+
+    function endGame (draw) {
+        if (draw){
+            selectors.winnerText.innerText = "It's a draw!"
+        } else {
+            selectors.winnerText.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+            circleTurn ? p2Score += 1 : p1Score += 1;
+            getScore();
         }
+        selectors.winnerScreen.classList.add('show');
+    }
+
+    function isDraw (){
+        return [...selectors.gameCells].every(cell => {
+            return cell.classList.contains(cellClasses.x) ||
+            cell.classList.contains(cellClasses.circle)
+        })
     }
 
     const handleClick = (event) => {
         const cell = event.target;
         const currentClass = circleTurn ? cellClasses.circle : cellClasses.x;
         placeMark(cell, currentClass);
-        (swapTurns = () => {
+        if (checkWin(currentClass)){
+            endGame(false);
+        } else if (isDraw()){
+            endGame(true)
+        } else {
+            (swapTurns = () => {
             circleTurn = !circleTurn
-        })()
+            })()
+        }
+    }
+
+    function getScore(){
+        selectors.p1Score.innerText = p1Score;
+        selectors.p2Score.innerText = p2Score;
     }
 
     selectors.gameCells.forEach(cell => {
-        cell.addEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, {once: true});
     })
-    
+
 })()
