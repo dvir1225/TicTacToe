d6cdd28f60bd6042accf062ac2d6b98e422825d1
Dvir <dvir1225@gmail.com>
2022-09-13 13:39:42 +0300

    add game function with mark placing and turn switching functionality

--- scripts.js
+++ scripts.js
@@ -8,3 +8,46 @@ const selectors = {
     p2Name: document.getElementById('p2Name').value,
     gameCells: document.querySelectorAll('.cell')
 }
+
+
+
+const Game = (() => {
+    const cellClasses = {
+        x: 'x',
+        circle: 'circle'
+    }
+    const gameMarks = {
+        x: '<i class="fa-solid fa-x"></i>',
+        circle: '<i class="fa-regular fa-circle"></i>'
+    }
+
+    let gameboard = [];
+    let player1 = selectors.p1Name;
+    let player2 = selectors.p2Name;
+    let circleTurn = false;
+
+    const placeMark = (cell, currentClass) => {
+        cell.classList.add(currentClass);
+        if (currentClass === cellClasses.circle){
+            cell.innerHTML = gameMarks.circle;
+            gameboard.push('o')
+        } else {
+            cell.innerHTML = gameMarks.x;
+            gameboard.push('x');
+        }
+    }
+
+    const handleClick = (event) => {
+        const cell = event.target;
+        const currentClass = circleTurn ? cellClasses.circle : cellClasses.x;
+        placeMark(cell, currentClass);
+        (swapTurns = () => {
+            circleTurn = !circleTurn
+        })()
+    }
+
+    selectors.gameCells.forEach(cell => {
+        cell.addEventListener('click', handleClick);
+    })
+    
+})()
